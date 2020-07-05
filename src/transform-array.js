d35718c27e8f77e050241bf71e783e011f904ce4
Roman Gukolov <roma.gykolov@yandex.com>
2020-07-05 14:07:51 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+const DISCARD_NEXT = '--discard-next';
+const DISCARD_PREV = '--discard-prev';
+const DOUBLE_NEXT = '--double-next';
+const DOUBLE_PREV = '--double-prev';
+module.exports = function transform(arr) {
+    const array = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === DISCARD_NEXT) {
+            i++;
+        } else if (arr[i] === DISCARD_PREV) {
+            if (arr[i - 2] !== DISCARD_NEXT) {
+                array.pop();
+            }
+        } else if (arr[i] === DOUBLE_PREV) {
+            if (i - 1 >= 0 && arr[i - 2] !== DISCARD_NEXT) {
+                array.push(arr[i - 1]);
+            }
+        } else if (arr[i] === DOUBLE_NEXT) {
+            if (i + 1 < arr.length) {
+                array.push(arr[i + 1]);
+            }
+        } else {
+            array.push(arr[i]);
+        }
+    }
+
+    return array;
+}
