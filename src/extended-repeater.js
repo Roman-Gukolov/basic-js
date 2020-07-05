8a98cb384ebf2d3c0904039132d4789d19873339
Roman Gukolov <roma.gykolov@yandex.com>
2020-07-05 14:08:19 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const DEFAULT_SEPARATOR = '+';
+const DEFAULT_ADDITION_SEPARATOR = '|';
+module.exports = function repeater(str, options) {
+    if (!options) {
+        return str;
+    }
+
+    if (options.addition !== undefined || typeof options.addition == 'boolean') {
+        let addition = String(options.addition);
+
+        if (options.additionRepeatTimes && options.additionRepeatTimes > 0) {
+            let additionSeparator = options.additionSeparator && options.additionRepeatTimes > 1 ? options.additionSeparator : DEFAULT_ADDITION_SEPARATOR;
+
+            for (let i = 1; i < options.additionRepeatTimes; i++) {
+                str += addition + additionSeparator;
+            }
+        }
+        str += addition;
+    }
+
+    let result = '';
+    if (options.repeatTimes && options.repeatTimes > 0) {
+        let separator = options.separator ? options.separator : DEFAULT_SEPARATOR;
+        for (let i = 1; i < options.repeatTimes; i++) {
+            result += str + separator;
+        }
+        result += str;
+    }
+    if (!options.repeatTimes && options.addition) {
+        return str;
+    }
+    return result;
+
 };
-  
